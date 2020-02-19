0ccea08f619b40d42d80732315ce60df93a63a61
ch <charis.rohmadi@gmail.com>
2020-02-19 16:10:05 +0700

    test 9

--- ICMSTU.API/Program.cs
+++ ICMSTU.API/Program.cs
@@ -8,7 +8,7 @@ namespace ICMSTU.API
     public static void Main(string[] args)
     {
       CreateWebHostBuilder(args).Build().Run();
-      //test 8
+      //test 9
     }
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args)
