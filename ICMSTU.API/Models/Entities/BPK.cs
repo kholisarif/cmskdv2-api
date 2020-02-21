80c20b1777cadc1d61007cc3b40bdd9cfa4f3a6d
Kholis Arifin <kholis_arifin@yahoo.co.id>
2020-02-21 13:01:50 +0700

    swagger

--- ICMSTU.API/Models/Entities/BPK.cs
+++ ICMSTU.API/Models/Entities/BPK.cs
@@ -8,15 +8,18 @@ namespace ICMSTU.API.Models.Entities
   [Table("BPK")]
   public class BPK
   {
+    [ForeignKey("DAFTUNIT")]
     public string UnitKey { get; set; }
     public DAFTUNIT DAFTUNIT { get; set; }
     public string NoBPK { get; set; }
+    [ForeignKey("STATTRS")]
     public string KdStatus { get; set; }
     public STATTRS STATTRS { get; set; }
     public bool? StPanjar { get; set; }
     public bool? StTunai { get; set; }
     public bool? StBank { get; set; }
     public int IdxKode { get; set; }
+    [ForeignKey("BEND")]
     public string KeyBend { get; set; }
     public BEND BEND { get; set; }
     [Column(TypeName="Date")]
@@ -25,15 +28,19 @@ namespace ICMSTU.API.Models.Entities
     public string UraiBPK { get; set; }
     [Column(TypeName="Date")]
     public DateTime? TglValid { get; set; }
+    [ForeignKey("Berita")]
     public string NoBA { get; set; }
     public Berita Berita { get; set; }
     public string KdBank { get; set; }
     public string NoRek { get; set; }
     public int Id { get; set; }
+    [ForeignKey("JTRANSFER")]
     public int? KdTransfer { get; set; }
     public JTRANSFER JTRANSFER { get; set; }
+    [ForeignKey("JBAYAR")]
     public int? KdBayar { get; set; }
     public JBAYAR JBAYAR { get; set; }
+    [ForeignKey("DAFTPHK")]
     public string KDP3 { get; set; }
     public DAFTPHK DAFTPHK { get; set; }
     public string NMP3 { get; set; }
@@ -41,8 +48,10 @@ namespace ICMSTU.API.Models.Entities
     public string NmBank { get; set; }
     public string NoRef { get; set; }
     public int? KdRilis { get; set; }
+    [ForeignKey("JKIRIM")]
     public int? StKirim { get; set; }
     public JKIRIM JKIRIM { get; set; }
+    [ForeignKey("JCAIR")]
     public int? StCair { get; set; }
     public JCAIR JCAIR { get; set; }
     [Column(TypeName="Date")]
--- ICMSTU.API/Models/Entities/Berita.cs
+++ ICMSTU.API/Models/Entities/Berita.cs
@@ -8,25 +8,28 @@ namespace ICMSTU.API.Models.Entities
   [Table("BERITA")]
   public class Berita
   {
+    [ForeignKey("DAFTUNIT")]
     public string UnitKey { get; set; }
-    // public DAFTUNIT DAFTUNIT { get; set; }
-    [Key]
+    public DAFTUNIT DAFTUNIT { get; set; }
     public string NoBA { get; set; }
     [Column(TypeName="Date")]
     public DateTime? TglBA { get; set; }
     public string KdKegUnit { get; set; }
+    [ForeignKey("Kontrak")]
     public string NoKon { get; set; }
     public Kontrak Kontrak { get; set; }
     public string UraiBA { get; set; }
     [Column(TypeName="Date")]
     public DateTime? TglValid { get; set; }
+    [ForeignKey("STATTRS")]
     public string KdStatus{ get; set; }
     public STATTRS STATTRS { get; set; }
+    [Key]
     public int Id { get; set; }
     public DateTime? DateCreate { get; set; }
     public DateTime? DateUpdate { get; set; }
     public string NoKontrak { get; set; }
     public string NoBerita{ get; set; }
-     public List<BPK> BPKs { get; set; }
+    public List<BPK> BPKs { get; set; }
   }
 }
