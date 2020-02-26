2639b103215c64e9b8196e7f0b6df6700920144d
Kholis Arifin <kholis_arifin@yahoo.co.id>
2020-02-26 15:08:58 +0700

    relasi BPKDETRDANA DAN JDANA

--- ICMSTU.API/Models/Entities/BPKDETRDANA.cs
+++ ICMSTU.API/Models/Entities/BPKDETRDANA.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ICMSTU.API.Models.Entities
 {
@@ -12,7 +13,12 @@ namespace ICMSTU.API.Models.Entities
     public string NoBPK { get; set; }
     public string KdDana { get; set; }
     public Decimal? Nilai { get; set; }
+    [Key]
     public int Id { get; set; }
+    public int IdJDANA { get; set; }
+    [ForeignKey("IdJDANA")]
+    public JDANA JDANA { get; set; }
+    // public DAFTPHK DAFTPHK { get; set; }
     public string NoJetra { get; set; }
     public DateTime? DateCreate { get; set; }
     public DateTime? DateUpdate { get; set; }
